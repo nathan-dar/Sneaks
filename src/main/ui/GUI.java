f7ef948278756603aa1dd067a0276f0b6079e5a3
ndar <ndar@students.cs.ubc.ca>
2020-11-03 21:32:21 -0800

    added functional save and load buttons to gui

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -1,7 +1,14 @@
 package ui;
 
+import model.Collection;
+import model.Sneaker;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -9,8 +16,11 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
+import static java.lang.Double.parseDouble;
+
 public class GUI {
 
+    private static final String JSON_STORE = "./data/collection.json";
     private static final int FRAME_WIDTH = 800;
     private static final int FRAME_HEIGHT = 400;
 
@@ -27,7 +37,13 @@ public class GUI {
     JTextField resell;
     JButton buttonAdd;
     JButton buttonRemove;
+    JButton buttonLoad;
+    JButton buttonSave;
     DefaultTableModel tableModel;
+
+    JsonWriter jsonWriter;
+    JsonReader jsonReader;
+    Collection collection;
 
     public GUI() {
         runSneaksGUI();
@@ -37,9 +53,16 @@ public class GUI {
     private void runSneaksGUI() {
         frame = new JFrame();
         table = new JTable();
+        collection = new Collection("User's Collection");
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
         initializeJTextFields();
         initializeButtonRemove();
         initializeButtonAdd();
+        initializeButtonSave();
+        initializeButtonLoad();
+
+        frame.setTitle("Sneaks");
 
         // create a table model and set a Column Identifiers to this model
         tableModel = new DefaultTableModel();
@@ -124,19 +147,93 @@ public class GUI {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                Object[] row = new Object[7];
-                row[0] = brand.getText().toUpperCase();
-                row[1] = model.getText().toUpperCase();
-                row[2] = colour.getText().toUpperCase();
-                row[3] = size.getText().toUpperCase();
-                row[4] = condition.getText().toUpperCase();
-                row[5] = retail.getText().toUpperCase();
-                row[6] = resell.getText().toUpperCase();
-
-                tableModel.addRow(row);
+                addSneakerFromFields();
                 clearTextFields();
             }
         });
+    }
+
+    // MODIFIES: this
+    // EFFECTS: gets text from fields and add it to the collection
+    private void addSneakerFromFields() {
+        Object[] row = new Object[7];
+        row[0] = brand.getText().toUpperCase();
+        row[1] = model.getText().toUpperCase();
+        row[2] = colour.getText().toUpperCase();
+        row[3] = size.getText();
+        row[4] = condition.getText();
+        row[5] = retail.getText();
+        row[6] = resell.getText();
+        String b = brand.getText().toUpperCase();;
+        String m = brand.getText().toUpperCase();;
+        String col = brand.getText().toUpperCase();;
+        Double s = parseDouble(size.getText());
+        Double c = parseDouble(condition.getText());
+        Double ret = parseDouble(retail.getText());
+        Double res = parseDouble(resell.getText());
+        collection.addSneaker(new Sneaker(b, m, col, s, c, ret, res));
+        tableModel.addRow(row);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes remove button
+    private void initializeButtonSave() {
+        buttonSave = new JButton("Save");
+        buttonSave.setBounds(500, 330, 100, 30);
+        frame.add(buttonSave);
+
+        buttonSave.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    jsonWriter.open();
+                    jsonWriter.write(collection);
+                    jsonWriter.close();
+                } catch (FileNotFoundException exception) {
+                    System.out.println("Unable to write to file: " + JSON_STORE);
+                }
+            }
+        });
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes add button
+    private void initializeButtonLoad() {
+        buttonLoad = new JButton("Load");
+        buttonLoad.setBounds(200, 330, 100, 30);
+        frame.add(buttonLoad);
+
+        buttonLoad.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loadCollection();
+            }
+        });
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads collection from file
+    private void loadCollection() {
+        try {
+            collection = jsonReader.read();
+            tableModel.setRowCount(0); // wipes the current table
+            for (int i = 0; i < collection.collectionSize(); i++) {
+                Object[] row = new Object[7];
+                Sneaker s = collection.getSneaker(i);
+                row[0] = s.getBrand();
+                row[1] = s.getModel();
+                row[2] = s.getColourway();
+                row[3] = s.getShoeSize();
+                row[4] = s.getCondition();
+                row[5] = s.getRetailValue();
+                row[6] = s.getResellValue();
+                tableModel.addRow(row);
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STORE);
+        }
     }
 
     // MODIFIES: this
