5b6b3a44920aeaff9009d50e79992f2e837d5b81
ndar <ndar@students.cs.ubc.ca>
2020-11-04 14:13:25 -0800

    changed certain sneaker fields to have a drop down menu for easier input

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -22,17 +22,19 @@ public class GUI {
     private static final int FRAME_HEIGHT = 400;
     private static final String[] COLUMNS =
             {"Brand", "Model", "ColourWay", "Size", "Condition", "Retail Value", "Resell Value"};
-    private static final String[] BRAND_OPTIONS = {"ASICS", "CONVERSE", "DESIGNER", "FILA", "JORDAN", "NEW BALANCE",
-            "NIKE", "REEBOK", "SKETCHERS", "UNDER ARMOUR", "VANS"};
+    private static final String[] BRAND_OPTIONS =
+            {"--BRAND--", "ADIDAS", "ASICS", "CONVERSE", "DESIGNER", "FILA", "JORDAN", "NEW BALANCE", "NIKE", "REEBOK",
+                    "SKETCHERS", "UNDER ARMOUR", "VANS"};
     private static final String[] SIZE_OPTIONS =
-            {"5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5",
-                    "13"};
+            {"--SIZE--", "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12",
+                    "12.5", "13"};
     private static final String[] CONDITION_OPTIONS =
-            {"0", "0.5", "1", "1.5", "2", "2.5", "3", "3.5", "4", "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9",
-                    "9.5", "10"};
+            {"--CONDITION--", "0", "0.5", "1", "1.5", "2", "2.5", "3", "3.5", "4", "4.5", "5", "5.5", "6", "6.5", "7",
+                    "7.5", "8", "8.5", "9", "9.5", "10"};
 
     private JFrame frame;
     private JTable table;
+    private DefaultTableModel tableModel;
     private JComboBox<String> brandChoice;
     private JTextField model;
     private JTextField colour;
@@ -40,12 +42,6 @@ public class GUI {
     private JComboBox<String> conditionChoice;
     private JTextField retail;
     private JTextField resell;
-    private JButton buttonAdd;
-    private JButton buttonRemove;
-    private JButton buttonLoad;
-    private JButton buttonSave;
-    private DefaultTableModel tableModel;
-
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
     private Collection collection;
@@ -57,17 +53,36 @@ public class GUI {
     // EFFECTS: runs the sneaks app gui
     private void runSneaksGUI() {
         frame = new JFrame();
-        table = new JTable();
         collection = new Collection("User's Collection");
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE);
+
+        int loadOnStart = startupPopup();
+
         initializeSneakerInputs();
         initializeButtonRemove();
         initializeButtonAdd();
         initializeButtonSave();
         initializeButtonLoad();
+        initializeTable();
 
+        if (loadOnStart == 0) {
+            loadCollection();
+        }
+
+        frame.setLayout(null);
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setTitle("Sneaks");
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes JTable, DefaultTableModel, and JScrollPane
+    private void initializeTable() {
+        table = new JTable();
 
         tableModel = new DefaultTableModel();
         tableModel.setColumnIdentifiers(COLUMNS);
@@ -76,23 +91,16 @@ public class GUI {
         JScrollPane pane = new JScrollPane(table);
         pane.setBounds(20, 20, FRAME_WIDTH - 40, 200);
         frame.add(pane);
-
-        frame.setLayout(null);
-        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
     }
 
     // MODIFIES: this
     // EFFECTS: initializes input boxes and selection boxes
     private void initializeSneakerInputs() {
-        brandChoice = new JComboBox<String>(BRAND_OPTIONS);
+        brandChoice = new JComboBox<>(BRAND_OPTIONS);
         model = new JTextField();
         colour = new JTextField();
-        sizeChoice = new JComboBox<String>(SIZE_OPTIONS);
-        conditionChoice = new JComboBox<String>(CONDITION_OPTIONS);
+        sizeChoice = new JComboBox<>(SIZE_OPTIONS);
+        conditionChoice = new JComboBox<>(CONDITION_OPTIONS);
         retail = new JTextField();
         resell = new JTextField();
 
@@ -116,7 +124,7 @@ public class GUI {
     // MODIFIES: this
     // EFFECTS: initializes remove button
     private void initializeButtonRemove() {
-        buttonRemove = new JButton("Remove");
+        JButton buttonRemove = new JButton("Remove");
         buttonRemove.setBounds(500, 300, 100, 30);
         frame.add(buttonRemove);
 
@@ -126,8 +134,13 @@ public class GUI {
             public void actionPerformed(ActionEvent e) {
                 if (table.getSelectedRow() >= 0) {
                     int i = table.getSelectedRow();
+                    Sneaker s = collection.getSneaker(i);
                     tableModel.removeRow(i);
                     collection.removeSneaker(i);
+                    JOptionPane.showMessageDialog(frame,
+                            s.getBrand() + " " + s.getModel() + " " + s.getColourway() + " has been removed.",
+                            "Sneaks - Sneaker Removed",
+                            JOptionPane.PLAIN_MESSAGE);
                 }
             }
         });
@@ -136,7 +149,7 @@ public class GUI {
     // MODIFIES: this
     // EFFECTS: initializes add button
     private void initializeButtonAdd() {
-        buttonAdd = new JButton("Add");
+        JButton buttonAdd = new JButton("Add");
         buttonAdd.setBounds(200, 300, 100, 30);
         frame.add(buttonAdd);
 
@@ -164,18 +177,23 @@ public class GUI {
         String b = row[0].toString();
         String m = model.getText().toUpperCase();
         String col = colour.getText().toUpperCase();
-        Double s = parseDouble(row[3].toString());
+        Double sz = parseDouble(row[3].toString());
         Double c = parseDouble(row[4].toString());
         Double ret = parseDouble(retail.getText());
         Double res = parseDouble(resell.getText());
-        collection.addSneaker(new Sneaker(b, m, col, s, c, ret, res));
+        Sneaker s = new Sneaker(b, m, col, sz, c, ret, res);
+        collection.addSneaker(s);
         tableModel.addRow(row);
+        JOptionPane.showMessageDialog(frame,
+                s.getBrand() + " " + s.getModel() + " " + s.getColourway() + " has been added.",
+                "Sneaks - Sneaker Added",
+                JOptionPane.PLAIN_MESSAGE);
     }
 
     // MODIFIES: this
     // EFFECTS: initializes save button
     private void initializeButtonSave() {
-        buttonSave = new JButton("Save");
+        JButton buttonSave = new JButton("Save");
         buttonSave.setBounds(500, 330, 100, 30);
         frame.add(buttonSave);
 
@@ -184,6 +202,10 @@ public class GUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 saveCollection();
+                JOptionPane.showMessageDialog(frame,
+                        "The collection has been saved.",
+                        "Sneaks - Collection Saved",
+                        JOptionPane.PLAIN_MESSAGE);
             }
         });
     }
@@ -203,7 +225,7 @@ public class GUI {
     // MODIFIES: this
     // EFFECTS: initializes add button
     private void initializeButtonLoad() {
-        buttonLoad = new JButton("Load");
+        JButton buttonLoad = new JButton("Load");
         buttonLoad.setBounds(200, 330, 100, 30);
         frame.add(buttonLoad);
 
@@ -212,6 +234,10 @@ public class GUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 loadCollection();
+                JOptionPane.showMessageDialog(frame,
+                        "The collection has been loaded.",
+                        "Sneaks - Collection Loaded",
+                        JOptionPane.PLAIN_MESSAGE);
             }
         });
     }
@@ -221,7 +247,7 @@ public class GUI {
     private void loadCollection() {
         try {
             collection = jsonReader.read();
-            tableModel.setRowCount(0); // wipes the current table
+            tableModel.setRowCount(0);
             for (int i = 0; i < collection.collectionSize(); i++) {
                 Object[] row = new Object[7];
                 Sneaker s = collection.getSneaker(i);
@@ -242,10 +268,22 @@ public class GUI {
     // MODIFIES: this
     // EFFECTS: clears text field boxes
     private void clearTextFields() {
-        model.setText("");
-        colour.setText("");
-        retail.setText("");
-        resell.setText("");
+        brandChoice.setSelectedIndex(0);
+        model.setText("--MODEL--");
+        colour.setText("--COLOURWAY--");
+        sizeChoice.setSelectedIndex(0);
+        conditionChoice.setSelectedIndex(0);
+        retail.setText("XXX.XX");
+        resell.setText("XXX.XX");
+    }
+
+    // EFFECTS: displays an startup message, return 0 if user selects "Yes", return 1 if user selects "No"
+    private int startupPopup() {
+        return JOptionPane.showConfirmDialog(
+                frame,
+                "Would you like to load the collection that is on file?",
+                "Sneaks - Welcome!",
+                JOptionPane.YES_NO_OPTION);
     }
 
 }
