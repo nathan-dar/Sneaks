cc14c95566fa4984dca07a123b74be3acb3dcf99
ndar <ndar@students.cs.ubc.ca>
2020-11-04 00:47:55 -0800

    changed certain sneaker fields to have a drop down menu for easier input

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -9,41 +9,46 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 import static java.lang.Double.parseDouble;
 
+// Represents the sneaks app GUI
 public class GUI {
 
     private static final String JSON_STORE = "./data/collection.json";
     private static final int FRAME_WIDTH = 800;
     private static final int FRAME_HEIGHT = 400;
+    private static final String[] COLUMNS =
+            {"Brand", "Model", "ColourWay", "Size", "Condition", "Retail Value", "Resell Value"};
+    private static final String[] BRAND_OPTIONS = {"ASICS", "CONVERSE", "DESIGNER", "FILA", "JORDAN", "NEW BALANCE",
+            "NIKE", "REEBOK", "SKETCHERS", "UNDER ARMOUR", "VANS"};
+    private static final String[] SIZE_OPTIONS =
+            {"5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5",
+                    "13"};
+    private static final String[] CONDITION_OPTIONS =
+            {"0", "0.5", "1", "1.5", "2", "2.5", "3", "3.5", "4", "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9",
+                    "9.5", "10"};
 
-    String[] columns = {"Brand", "Model", "ColourWay", "Size", "Condition [X/10]", "Retail Value", "Resell Value"};
+    private JFrame frame;
+    private JTable table;
+    private JComboBox<String> brandChoice;
+    private JTextField model;
+    private JTextField colour;
+    private JComboBox<String> sizeChoice;
+    private JComboBox<String> conditionChoice;
+    private JTextField retail;
+    private JTextField resell;
+    private JButton buttonAdd;
+    private JButton buttonRemove;
+    private JButton buttonLoad;
+    private JButton buttonSave;
+    private DefaultTableModel tableModel;
 
-    JFrame frame;
-    JTable table;
-    JTextField brand;
-    JTextField model;
-    JTextField colour;
-    JTextField size;
-    JTextField condition;
-    JTextField retail;
-    JTextField resell;
-    JButton buttonAdd;
-    JButton buttonRemove;
-    JButton buttonLoad;
-    JButton buttonSave;
-    DefaultTableModel tableModel;
-
-    JsonWriter jsonWriter;
-    JsonReader jsonReader;
-    Collection collection;
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
+    private Collection collection;
 
     public GUI() {
         runSneaksGUI();
@@ -56,7 +61,7 @@ public class GUI {
         collection = new Collection("User's Collection");
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE);
-        initializeJTextFields();
+        initializeSneakerInputs();
         initializeButtonRemove();
         initializeButtonAdd();
         initializeButtonSave();
@@ -64,50 +69,46 @@ public class GUI {
 
         frame.setTitle("Sneaks");
 
-        // create a table model and set a Column Identifiers to this model
         tableModel = new DefaultTableModel();
-        tableModel.setColumnIdentifiers(columns);
-
-        // set the model to the table
+        tableModel.setColumnIdentifiers(COLUMNS);
         table.setModel(tableModel);
 
-        // create JScrollPane
         JScrollPane pane = new JScrollPane(table);
         pane.setBounds(20, 20, FRAME_WIDTH - 40, 200);
-
-        frame.setLayout(null);
         frame.add(pane);
 
+        frame.setLayout(null);
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
     }
 
     // MODIFIES: this
-    // EFFECTS: initializes text fields
-    private void initializeJTextFields() {
-        brand = new JTextField();
+    // EFFECTS: initializes input boxes and selection boxes
+    private void initializeSneakerInputs() {
+        brandChoice = new JComboBox<String>(BRAND_OPTIONS);
         model = new JTextField();
         colour = new JTextField();
-        size = new JTextField();
-        condition = new JTextField();
+        sizeChoice = new JComboBox<String>(SIZE_OPTIONS);
+        conditionChoice = new JComboBox<String>(CONDITION_OPTIONS);
         retail = new JTextField();
         resell = new JTextField();
 
-        brand.setBounds(20, 220, 100, 30);
+        brandChoice.setBounds(20, 220, 100, 30);
         model.setBounds(130, 220, 100, 30);
         colour.setBounds(240, 220, 100, 30);
-        size.setBounds(350, 220, 100, 30);
-        condition.setBounds(460, 220, 100, 30);
+        sizeChoice.setBounds(350, 220, 100, 30);
+        conditionChoice.setBounds(460, 220, 100, 30);
         retail.setBounds(570, 220, 100, 30);
         resell.setBounds(680, 220, 100, 30);
 
-        frame.add(brand);
+        frame.add(brandChoice);
         frame.add(model);
         frame.add(colour);
-        frame.add(size);
-        frame.add(condition);
+        frame.add(sizeChoice);
+        frame.add(conditionChoice);
         frame.add(retail);
         frame.add(resell);
     }
@@ -124,7 +125,9 @@ public class GUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (table.getSelectedRow() >= 0) {
-                    tableModel.removeRow(table.getSelectedRow());
+                    int i = table.getSelectedRow();
+                    tableModel.removeRow(i);
+                    collection.removeSneaker(i);
                 }
             }
         });
@@ -151,18 +154,18 @@ public class GUI {
     // EFFECTS: gets text from fields and add it to the collection
     private void addSneakerFromFields() {
         Object[] row = new Object[7];
-        row[0] = brand.getText().toUpperCase();
+        row[0] = brandChoice.getSelectedItem();
         row[1] = model.getText().toUpperCase();
         row[2] = colour.getText().toUpperCase();
-        row[3] = size.getText();
-        row[4] = condition.getText();
+        row[3] = sizeChoice.getSelectedItem();
+        row[4] = conditionChoice.getSelectedItem();
         row[5] = retail.getText();
         row[6] = resell.getText();
-        String b = brand.getText().toUpperCase();;
-        String m = brand.getText().toUpperCase();;
-        String col = brand.getText().toUpperCase();;
-        Double s = parseDouble(size.getText());
-        Double c = parseDouble(condition.getText());
+        String b = row[0].toString();
+        String m = model.getText().toUpperCase();
+        String col = colour.getText().toUpperCase();
+        Double s = parseDouble(row[3].toString());
+        Double c = parseDouble(row[4].toString());
         Double ret = parseDouble(retail.getText());
         Double res = parseDouble(resell.getText());
         collection.addSneaker(new Sneaker(b, m, col, s, c, ret, res));
@@ -239,11 +242,8 @@ public class GUI {
     // MODIFIES: this
     // EFFECTS: clears text field boxes
     private void clearTextFields() {
-        brand.setText("");
         model.setText("");
         colour.setText("");
-        size.setText("");
-        condition.setText("");
         retail.setText("");
         resell.setText("");
     }
